decc3a5187636ce3fb7f7198e79232364cb39028
Usama-ak-ai <usamaakhtar767@gmail.com>
2025-10-10 23:11:26 +0500

    first day

---
+++ app/components/Navbar.tsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import { Link } from 'react-router';
+
+
+
+const Navbar=()=>{
+  return (
+    <nav className='navbar'>
+      <Link to='/'>
+      <p className='text-2*l font-bold text-gradient'>
+        Resumind
+      </p>
+      </Link>
+      <Link to='/upload'>
+      <p className='primary-button w-fit'>
+        Upload
+      </p>
+      </Link>
+
+    </nav>
+  )
+
+}
+
+export default Navbar;
--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,13 +1,38 @@
 import type { Route } from "./+types/home";
-import { Welcome } from "../welcome/welcome";
+import Navbar from "~/components/Navbar";
+import resumes from "../constants/index"
+
 
 export function meta({}: Route.MetaArgs) {
   return [
-    { title: "New React Router App" },
-    { name: "description", content: "Welcome to React Router!" },
+    { title: "Resumind" },
+    { name: "description", content: "Smart feedback for our homepage" },
   ];
 }
 
 export default function Home() {
-  return <Welcome />;
+  return <>
+  <main className="bg-[url('/images/bg-main.svg')] bg-cover">
+    <section className="main-section">
+      <Navbar/>
+      <div className="page-heading">
+        <h1>Track Your Applications And Resume Ratings</h1>
+        <h2>
+          Review your submissions and check AI-powered apps
+        </h2>
+
+      </div>
+    </section>
+    {resumes.length >0 && 
+  (
+    <div className="resumes-section">
+      {resumes.map(callbackfn:(resume)=>)}
+
+    </div>
+  )}
+  </main>
+  
+  
+  
+  </>;
 }
